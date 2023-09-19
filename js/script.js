fa0d7cc33bcdb76fe0cbf02693fd4c049ddae325
Domitrii <126395589+Domitrii@users.noreply.github.com>
2023-09-19 13:53:27 +0200

    d

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const openButton = document.getElementById("openButton");
+    const closeButton = document.getElementById("closeButton");
+    const overlay = document.getElementById("overlay");
+
+    openButton.addEventListener("click", function () {
+        overlay.classList.add("is-open");
+    });
+
+    closeButton.addEventListener("click", function () {
+        overlay.classList.remove("is-open")
+    });
+});
